156036b9ce1700cc249d6234780189f8d3a8e8cb
Christian Franke <chriss@frankeonline.net>
2020-05-08 22:02:03 +0200

    better counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import styled from 'styled-components';
+import { createMachine, assign } from 'xstate';
+import { useMachine } from '@xstate/react';
 
-const Button = styled.button`
-  background: palevioletred;
-  border-radius: 3px;
-  border: none;
-  color: white;
-`
+
+type DnDContext = {
+  counter: number
+}
+
+type CountEvent = {type: 'COUNT'}
+type ResetEvent = {type: 'RESET'}
+
+type DnDEvents = CountEvent | ResetEvent
+
+type DnDState = {
+  value: "Init"
+  context: DnDContext & {
+    counter: 0
+  }
+} | {
+  value: "Counting"
+  context: DnDContext
+} | {
+  value: "Max"
+  context: DnDContext
+}
+
+
+export const machine = createMachine<DnDContext, DnDEvents, DnDState>({
+  id: "dnd",
+  initial: "Init",
+  states: {
+    Init: {
+      entry: 'reset',
+      on: {
+        'COUNT': {
+          target: 'Counting',
+        }
+      }
+    },
+    Counting:{
+      entry: 'count',
+      on: {
+
+        'RESET': {
+          target: 'Init',
+          internal: false
+        },
+        'COUNT': [{
+          target: 'Max',
+          cond: 'isMax',
+        },
+        {
+          target: 'Counting',
+          internal: false
+        }
+      ]
+      } 
+    },
+    Max: {
+      on: {
+        'RESET': {
+          target: 'Init',
+          internal: false
+        }
+        
+      }
+    },
+  }
+}, {
+  guards: {
+    isMax: (ctx: DnDContext)=> ctx.counter > 5
+  },
+  actions: {
+    count: assign<DnDContext>({counter: (ctx) => ctx.counter + 1}),
+    reset: assign<DnDContext>({counter: (ctx) => 0})
+  }
+})
 
 function App() {
-  const [counter,setCounter] = useState(0)
-
+  const [state, send] = useMachine(machine, {devTools: true})
   return (
     <div className="App" data-testid="counter-screen">
       <header className="App-header">
 
-        <Button>Test</Button>
-        <div>Counter: <span data-testid="value-counter">{counter}</span></div>
-        <button data-testid="button-count" onClick={()=> setCounter(counter + 1)}>count</button>
-        <button data-testid="button-reset" onClick={()=> setCounter(0)}>reset</button>
+        <div>Counter: <span data-testid="value-counter">{state.context.counter}</span></div>
+        <button disabled={state.matches("Max")} data-testid="button-count" onClick={()=> send({type: 'COUNT'})}>count</button>
+        <button disabled={state.matches("Init")} data-testid="button-reset" onClick={()=> send({type: 'RESET'})}>reset</button>
       </header>
     </div>
   );
